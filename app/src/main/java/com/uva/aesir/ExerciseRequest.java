bd4a5a7f60bb4a78fd578a118cca497cedc7b2f4
Marc <marc-gw@live.nl>
2019-01-09 14:03:09 +0100

    hopefully working again

--- app/src/main/java/com/uva/aesir/ExerciseRequest.java
+++ app/src/main/java/com/uva/aesir/ExerciseRequest.java
@@ -1,10 +1,18 @@
 package com.uva.aesir;
 
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 
 import org.json.JSONObject;
 
 public class ExerciseRequest implements Response.Listener<JSONObject>, Response.ErrorListener {
+    @Override
+    public void onErrorResponse(VolleyError error) {
 
+    }
 
+    @Override
+    public void onResponse(JSONObject response) {
+
+    }
 }
